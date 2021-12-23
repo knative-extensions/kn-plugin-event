46e4f01d9c2666fef13ec73728da1ea98e84849c
Chris Suszynski <ksuszyns@redhat.com>
2021-12-23 14:09:28 +0100

    Using proper image (#132)

--- pkg/cli/ics/constants.go
+++
@@ -1,5 +0,0 @@
-package ics
-
-// ContainerBasename holds a OCI container base name.
-// TODO: set value from .env file during the build.
-var ContainerBasename = "quay.io/cardil" //nolint:gochecknoglobals
